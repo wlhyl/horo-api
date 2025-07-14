311751998e43ebc930de68eb38e27165bbf0b0e8
lzh <1216083447@qq.com>
2025-07-15 02:47:53 +0800

    为qizheng lunar_mansions补充一些单元测试用例 为calc_distance_star_long编写快照测试

--- qizheng/src/lunar_mansions.rs
+++ qizheng/src/lunar_mansions.rs
@@ -1,4 +1,4 @@
-use swe::{swe_close, swe_degnorm, swe_fixstar2_ut, swe_set_ephe_path, Flag};
+use swe::{Flag, swe_close, swe_degnorm, swe_fixstar2_ut, swe_set_ephe_path};
 
 use crate::{config::DistanceStarConfig, error::Error};
 
@@ -9,9 +9,9 @@ use serde::Serialize;
 use utoipa::ToSchema;
 
 /// 二十八宿名
-#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 #[cfg_attr(feature = "serde", derive(Serialize))]
 #[cfg_attr(feature = "swagger", derive(ToSchema))]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub enum LunarMansionsName {
     // 东方七宿
     角,
@@ -107,7 +107,11 @@ pub(crate) fn calc_xiu_degree(
 mod tests {
     use swe::swe_degnorm;
 
-    use crate::{lunar_mansions::calc_xiu_degree, DistanceStarLong, LunarMansionsName::*};
+    use crate::{
+        DistanceStarConfig, DistanceStarLong,
+        LunarMansionsName::*,
+        lunar_mansions::{calc_distance_star_long, calc_xiu_degree},
+    };
 
     #[test]
     fn test_calc_xiu_degree() {
@@ -323,5 +327,180 @@ mod tests {
         let (xiu, xiu_degree) = xiu_and_degree.unwrap();
         assert_eq!(xiu, 翼);
         assert_eq!(xiu_degree, 6.0);
+    }
+
+    #[test]
+    fn test_calc_xiu_degree_full_coverage() {
+        let distance_star_long: [DistanceStarLong; 28] = [
+            DistanceStarLong {
+                lunar_mansions: 角,
+                long: swe_degnorm(180.0 + 12.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 亢,
+                long: swe_degnorm(180.0 + 24.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 氐,
+                long: swe_degnorm(180.0 + 36.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 房,
+                long: swe_degnorm(180.0 + 48.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 心,
+                long: swe_degnorm(180.0 + 60.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 尾,
+                long: swe_degnorm(180.0 + 72.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 箕,
+                long: swe_degnorm(180.0 + 84.0 + 1.0),
+            },
+            //
+            DistanceStarLong {
+                lunar_mansions: 斗,
+                long: swe_degnorm(180.0 + 96.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 牛,
+                long: swe_degnorm(180.0 + 108.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 女,
+                long: swe_degnorm(180.0 + 120.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 虚,
+                long: swe_degnorm(180.0 + 132.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 危,
+                long: swe_degnorm(180.0 + 144.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 室,
+                long: swe_degnorm(180.0 + 156.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 壁,
+                long: swe_degnorm(180.0 + 168.0 + 1.0),
+            },
+            //
+            DistanceStarLong {
+                lunar_mansions: 奎,
+                long: swe_degnorm(180.0 + 180.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 娄,
+                long: swe_degnorm(180.0 + 192.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 胃,
+                long: swe_degnorm(180.0 + 204.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 昴,
+                long: swe_degnorm(180.0 + 216.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 毕,
+                long: swe_degnorm(180.0 + 228.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 觜,
+                long: swe_degnorm(180.0 + 240.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 参,
+                long: swe_degnorm(180.0 + 252.0 + 1.0),
+            },
+            //
+            DistanceStarLong {
+                lunar_mansions: 井,
+                long: swe_degnorm(180.0 + 264.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 鬼,
+                long: swe_degnorm(180.0 + 276.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 柳,
+                long: swe_degnorm(180.0 + 288.0 + 1.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 星,
+                long: swe_degnorm(180.0 + 300.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 张,
+                long: swe_degnorm(180.0 + 312.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 翼,
+                long: swe_degnorm(180.0 + 324.0),
+            },
+            DistanceStarLong {
+                lunar_mansions: 轸,
+                long: swe_degnorm(180.0 + 336.0),
+            },
+        ];
+
+        // Test the boundary of each mansion
+        for (index, star) in distance_star_long.iter().enumerate() {
+            // Test the exact start of the mansion
+            let (xiu, xiu_degree) = calc_xiu_degree(star.long, &distance_star_long).unwrap();
+            assert_eq!(
+                xiu, star.lunar_mansions,
+                "Failed at mansion {:?}",
+                star.lunar_mansions
+            );
+            // Due to f64 precision, we check if it's very close to 0
+            assert!(
+                xiu_degree < 1e-9,
+                "Failed at mansion {:?} with degree {}",
+                star.lunar_mansions,
+                xiu_degree
+            );
+
+            // Test the point just before the start of the mansion
+            let prev_star_index = (index + 28 - 1) % 28;
+            let prev_star = &distance_star_long[prev_star_index];
+            let point_before = swe_degnorm(star.long - 1e-9);
+            let (xiu, xiu_degree) = calc_xiu_degree(point_before, &distance_star_long).unwrap();
+
+            let prev_star_span = swe_degnorm(star.long - prev_star.long);
+
+            assert_eq!(
+                xiu, prev_star.lunar_mansions,
+                "Failed at point before mansion {:?}",
+                star.lunar_mansions
+            );
+            assert!(
+                // 由于f64精度问题，这里需要放宽判断，
+                // 实际计算结果，这个绝对值大于1e-9
+                (xiu_degree - prev_star_span).abs() < 1e-8,
+                "Failed at point before mansion {:?} with degree {}, expected {}",
+                star.lunar_mansions,
+                xiu_degree,
+                prev_star_span
+            );
+        }
+    }
+
+    #[test]
+    fn test_calc_distance_star_long_snapshot() {
+        dotenvy::dotenv().ok();
+        let ephe_path = std::env::var("EPHE_PATH")
+            .expect("没设置 EPHE_PATH 环境变量，可在.env文件中设置或export EPHE_PATH=...");
+
+        let distance_star_config = DistanceStarConfig::default_all_configs();
+        let jd_utc = 2451545.0; // J2000.0
+
+        let result = calc_distance_star_long(jd_utc, &distance_star_config, &ephe_path).unwrap();
+        insta::assert_yaml_snapshot!(result);
     }
 }
